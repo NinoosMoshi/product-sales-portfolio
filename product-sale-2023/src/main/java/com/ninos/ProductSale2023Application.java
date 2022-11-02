a9cba65ec92931b5b9b5ea7b5716b562d87172c5
NinoosMoshi <nalkaton@gmail.com>
2022-11-02 07:41:50 -0400

    inti project

---
+++ product-sale-2023/src/main/java/com/ninos/ProductSale2023Application.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSale2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSale2023Application.class, args);
+	}
+
+}
---
+++ product-sale-2023/src/main/java/com/ninos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProductSale2023Application.class);
+	}
+
+}
